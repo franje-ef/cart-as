d6e11dc8709bf2cc3713d7f43bac6bdd483a4094
Francisco Escalona <Francisco.Escalona@ding.com>
2020-05-19 17:02:59 +0100

    Added avatars page

---
+++ Cartas.Web/Controllers/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Linq;
+using System.Web.Mvc;
+using Cartas.Web.Models;
+
+namespace Cartas.Web.Controllers
+{
+    public class PlayerController : Controller
+    {
+        public ActionResult Register()
+        {
+            var model = new RegisterPlayerViewModel();
+            model.AvatarList = Directory.GetFiles(Server.MapPath("~/Content/img/avatars"))
+                .Select(Path.GetFileName)
+                .ToList();
+
+            return View(model);
+        }
+
+        //public ActionResult Register(RegisterPlayerViewModel model)
+        //{
+
+        //}
+    }
+}
---
+++ Cartas.Web/Models/RegisterPlayerViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Cartas.Web.Models
+{
+    public class RegisterPlayerViewModel
+    {
+        public string Name { get; set; }
+        public string AvatarId { get; set; }
+        public List<string> AvatarList { get; set; }
+    }
+}
