5cd40ae3baa5b9d7603980aa41c0df0926cfbd51
jesusescalonaf@gmail.com <jesusescalonaf@gmail.com>
2020-05-29 15:00:32 +0100

    migrated romi to ts

--- Cartas.Web/Scripts_ts/game/romi.ts
+++ Cartas.Web/Scripts_ts/game/romi.ts
@@ -1,5 +1,12 @@
 ﻿class Romi {
-    constructor(game, isMasterUser, resources, gameHubSender) {
+    game: Game;
+    isMasterUser: boolean;
+    resources: any;
+    gameHubSender: GameHubSender;
+    container: PIXI.Container;
+    goal: any;
+
+    constructor(game: Game, isMasterUser:boolean, resources, gameHubSender: GameHubSender) {
         this.game = game;
         this.isMasterUser = isMasterUser;
         this.resources = resources;
@@ -10,29 +17,11 @@
         var container = new PIXI.Container();
         this.container = container;
 
-        this._addBow(container, this.isMasterUser, this.resources, this.gameHubSender);
-        this._addTarget(container, this.resources);
-        this._addGoal(container, initialGoal, this);
+        this.addBow(container, this.isMasterUser, this.resources, this.gameHubSender);
+        this.addTarget(container, this.resources);
+        this.addGoal(container, initialGoal, this);
         this.game.stage.addChild(container);
         this.locateContainer(this);
-
-
-        /**
-         * /TESTS
-         */
-        //var self = this;
-        //function timeout() {
-        //    setTimeout(function () {
-        //        var randomGoal = Math.floor(Math.random() * (4 - 0 + 1)) + 0;
-        //        var goals = ["1 trío y 1 seguidilla", "3 tríos", "2 seguidillas", "2 tríos y 1 seguidilla", "libre"];
-        //        self.onGoalChanged(self, goals[randomGoal]);
-
-        //        timeout();
-        //    }, 1500);
-        //}
-
-        //timeout();
-        /*END TESTS*/
     }
 
     onGoalChanged(text) {
@@ -55,7 +44,7 @@
         self.container.y = 200;
     }
 
-    _addBow(container, isMasterUser, resources, gameHubSender) {
+    private addBow(container, isMasterUser, resources, gameHubSender) {
         if (isMasterUser) {
             var bow = new PIXI.Sprite(resources.bow.texture);
             bow.height = 64;
@@ -64,28 +53,36 @@
 
             bow.interactive = true;
             bow.buttonMode = true;
-
-            bow.mousedown = bow.touchstart = function (event) {
-                gameHubSender.changeGoal();
-
-                var button = event.target;
-                var original = button.height;
-
-                button.height = original * 2;
-                button.width = original * 2;
-
-                TweenLite.to(button, 0.4, {
-                    height: original,
-                    width: original,
-                    ease: Elastic.easeOut
+            bow.on("mousedown",
+                event => {
+                    this.changeGoalClicked(event, this.gameHubSender);
                 });
-            }
+            bow.on("touchstart",
+                event => {
+                    this.changeGoalClicked(event, this.gameHubSender);
+                });
 
             container.addChild(bow);
         }
     }
 
-    _addTarget(container, resources) {
+    private changeGoalClicked(event, gameHubSender: GameHubSender) {
+        gameHubSender.changeGoal();
+
+        var button = event.target;
+        var original = button.height;
+
+        button.height = original * 2;
+        button.width = original * 2;
+
+        TweenLite.to(button, 0.4, {
+            height: original,
+            width: original,
+            ease: Elastic.easeOut
+        });
+    }
+
+    private addTarget(container, resources) {
         var target = new PIXI.Sprite(resources.target.texture);
         target.height = 64;
         target.width = 64;
@@ -93,7 +90,7 @@
         container.addChild(target);
     }
 
-    _addGoal(container, initialGoal, self) {
+    private addGoal(container, initialGoal, self) {
         var goal = new PIXI.Text(initialGoal,
             {
                 fontFamily: 'Cabin Sketch',
