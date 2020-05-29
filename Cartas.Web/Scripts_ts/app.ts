b200e1f3ac890dbbe43a35115a951330b3edd492
jesusescalonaf@gmail.com <jesusescalonaf@gmail.com>
2020-05-29 07:41:25 +0100

    Adding typescript

---
+++ Cartas.Web/Scripts_ts/app.ts
@@ -0,0 +1,8 @@
+﻿function welcome(person: string) {
+    return "<h2>Hello " + person + ", Lets learn TypeScript</h2>";
+}
+
+function clickMeButton() {
+    let user: string = "MithunVP";
+    document.getElementById("divMsg").innerHTML = welcome(user);
+}
