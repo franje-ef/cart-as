755dd1cafaa49082afd8b5a4cec09f0a1938fa27
jesusescalonaf@gmail.com <jesusescalonaf@gmail.com>
2020-05-29 16:18:05 +0100

    migrated gameHub

---
+++ Cartas.Web/Scripts_ts/game/gameHubReceiver.ts
@@ -0,0 +1,55 @@
+﻿class GameHubReceiver {
+    gameHub: GameHub;
+    players: Players;
+    romi: Romi;
+    playedCard: PlayedCard;
+    reactionService: ReactionService;
+    confirmVictory: ConfirmVictory;
+
+    constructor(gameHub : GameHub, players: Players, romi: Romi, playedCard: PlayedCard, reactionService: ReactionService, confirmVictory: ConfirmVictory) {
+        this.gameHub = gameHub;
+        this.players = players;
+        this.romi = romi;
+        this.playedCard = playedCard;
+        this.reactionService = reactionService;
+        this.confirmVictory = confirmVictory;
+    }
+
+    init() {
+        this.gameHub.hub.client.playerConnected = player => {
+            this.players.addPlayer(player.PlayerId, player.PlayerName, '/Content/img/avatars/' + player.Avatar, player.Seat, player.WinCount);
+        }
+
+        this.gameHub.hub.client.onGameStarted = () => {
+            location.reload();
+        }
+
+        this.gameHub.hub.client.onGoalChanged = goal => {
+            this.romi.onGoalChanged(goal);
+        }
+
+        this.gameHub.hub.client.onCardPlayed = (seat, num, suit) => {
+            this.playedCard.setPlayedCard(seat, num, suit);
+        }
+
+        this.gameHub.hub.client.onTurnChanged = seat => {
+            this.players.setCurrentTurn(seat); 
+        }
+
+        this.gameHub.hub.client.onPlayedCardTaken = () => {
+            this.playedCard.removePlayedCard();
+        }
+
+        this.gameHub.hub.client.onReactionSent = (seat, reactionId) => {
+            this.reactionService.onReactionSent(seat, reactionId);
+        }
+
+        this.gameHub.hub.client.onPlayerRemoved = (seat, removedPlayerId) => {
+            this.players.onPlayerRemoved(seat, removedPlayerId);
+        }
+
+        this.gameHub.hub.client.onPlayerClaimedVictory = playerName => {
+            this.confirmVictory.show(playerName);
+        }
+    }
+}
